00fec790b417176552692a6763336d4c3051f503
Ben Borovinsky <bmborovinsky@gmail.com>
2024-09-08 15:37:25 -0700

    Including JS for "Read more" button

--- main.js
+++ main.js
@@ -105,3 +105,17 @@ $(function() {
         });
     }
 })();
+
+// Code for Read More/Less button
+function changeReadMore() { 
+    const mycontent = document.getElementById('mybox1id'); 
+    const mybutton = document.getElementById('mybuttonid'); 
+  
+    if (mycontent.style.display === 'none' || mycontent.style.display === '') { 
+        mycontent.style.display = 'block'; 
+        mybutton.textContent = 'Read Less'; 
+    } else { 
+        mycontent.style.display = 'none'; 
+        mybutton.textContent = 'Read More'; 
+    } 
+}
